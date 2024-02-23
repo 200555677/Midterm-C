3d145f63163115a3989e95cf1882dfc5320bc096
200555677 <babariyashakshi1211@gmail.com>
2024-02-22 18:39:30 -0800

    midterm submission

---
+++ Models/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace _200555677
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Models.Student> Students { get; set; }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _200555677.Models
+{
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Email Address")]
+        [EmailAddress]
+        public string EmailAddress { get; set; } // Optional field
+    }
+}
